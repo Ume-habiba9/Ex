5a67b7360cd52b3b10fd8e67853bc154e864522f
Ume-habiba9 <habibaume571@gmail.com>
2023-01-10 15:26:33 +0500

    concurrency added

--- ex.go
+++ ex.go
@@ -4,76 +4,70 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"sync"
 	"time"
 )
 
-func countWords(totalWords string) int {
+func countWords(totalWords string, wg *sync.WaitGroup) int {
 	words := strings.Fields(totalWords)
-	return len(words)
+	w, _ := fmt.Println("Number of words:", len(words))
+	wg.Done()
+	return w
 }
 
-func countLines(totalLines string) int {
+func countLines(totalLines string, wg *sync.WaitGroup) int {
 	lines := strings.Split(totalLines, "\n")
-	return len(lines)
+	l, _ := fmt.Println("Number of Lines:", len(lines))
+	wg.Done()
+	return l
 }
-func countVowels(char int32) bool {
+func countVowelsandPunc(file string, wg *sync.WaitGroup) (int, int) {
+	countVowel := 0
+	countPunc := 0
+	for _, char := range file {
+		if (char == 'a') || (char == 'e') || (char == 'i') ||
+			(char == 'o') || (char == 'u') {
+			countVowel++
 
-	if (char == 'a') || (char == 'e') || (char == 'i') ||
-		(char == 'o') || (char == 'u') {
-
-		return true
-
-	} else {
-
-		return false
-
+		}
 	}
-
-}
-func countPunc(p int32) bool {
-
-	if (p == '!') || (p == '.') || (p == '-') ||
-		(p == ',') || (p == ';') || (p == ':') {
-
-		return true
-
-	} else {
-
-		return false
-
+	v, _ := fmt.Println("No of Vowels:", countVowel)
+	for _, p := range file {
+		if (p == '!') || (p == '.') || (p == '-') ||
+			(p == ',') || (p == ';') || (p == ':') {
+			countPunc++
+		}
 	}
-
+	p, _ := fmt.Println("No of Puntuation:", countPunc)
+	wg.Done()
+	return v, p
 }
 
 func main() {
 	startTime := time.Now()
+	var wg sync.WaitGroup
 	fileContent, err := os.ReadFile("text.txt")
 	if err != nil {
 		fmt.Println(err)
 	}
 	file := string(fileContent)
 	// No of Words
-	fmt.Println("Number of words:", countWords(file))
+	wg.Add(1)
+	go countWords(file, &wg)
+	wg.Wait()
 	// No of lines
-	fmt.Println("Number of Lines:", countLines(file))
+	wg.Add(1)
+	go countLines(file, &wg)
+	wg.Wait()
 	// No of characters
 	fmt.Println("Number of characters:", len(file))
 	// No of Vowels
-	countvowels := 0
-	for _, char := range file {
-		if countVowels(char) {
-			countvowels++
-		}
-	}
-	fmt.Println("No of vowels :", countvowels)
-	// No of Puntuation
-	countpunc := 0
-	for _, punctuation := range file {
-		if countPunc(punctuation) {
-			countpunc++
-		}
-	}
-	fmt.Println("No of puntuations:", countpunc)
+	wg.Add(1)
+	go countVowelsandPunc(file, &wg)
+	wg.Wait()
+	// No of Puntuations
+	go countVowelsandPunc(file, &wg)
 	ExecutionTime := time.Since(startTime)
 	fmt.Println("Execution Time of Whole Program is :", ExecutionTime)
+	wg.Wait()
 }
